3e9e1ef0c238626003029d98c2f19863d461e10f
21grahams <graham.kirsh@gmail.com>
2021-06-10 11:52:40 -0600

    added init contact information

---
+++ portfolio/src/contents/Contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Social from "../components/Social";
+
+function Contact() {
+  return (
+    <div className="condiv">
+      <h1 className="subtopic">Contact Me</h1>
+      <h3>Email : graham.kirsh@gmail.com</h3>
+      <h3>Github : github.com/21grahams/</h3>
+      <h3>LinkedIn : www.linkedin.com/in/graham-kirsh</h3>
+      <Social />
+    </div>
+  );
+}
+export default Contact;
