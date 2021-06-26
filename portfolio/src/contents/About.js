fef2853c0c383f065230214c3fe4ec3a2eb09bca
21grahams <graham.kirsh@gmail.com>
2021-06-26 16:08:36 -0600

    split paragraphs in about section

--- portfolio/src/contents/About.js
+++ portfolio/src/contents/About.js
@@ -11,7 +11,9 @@ function About() {
       >
         View My Resume!
       </a>
-      <h4 style={{fontSize: '25px'}} className="subtopic">Hey there,</h4>
+      <h4 style={{ fontSize: "25px" }} className="subtopic">
+        Hey there,
+      </h4>
       <h1>I'm Graham Kirsh</h1>
       <h3>
         Full Stack <u>Software Developer</u> | UI/UX <u>Designer</u>
@@ -22,14 +24,18 @@ function About() {
         Colorado. Having built many applications, both individually and as part
         of teams, I thoroughly enjoy seeing the front page of a website
         transform into something elegant and beautiful. I also enjoy
-        optimization, scaling and working with data. Some of my projects include
-        the building of a page for an e-commerce website, a comprehensive API
-        that satisfies all needed requests, an app dedicated to providing
-        up-to-date and quality photographs directly from NASA's API and a
-        company events application to create, join and organize internal events
-        for employees. I would like to use my engineering skills to build
-        meaningful solutions to help sustain a healthier and better future for
-        planet Earth and humanity.
+        optimization, scaling and working with data.
+      </p>
+      <br></br>
+      <p>
+        {" "}
+        Some of my projects include the building of a page for an e-commerce
+        website, a comprehensive API that satisfies all needed requests, an app
+        dedicated to providing up-to-date and quality photographs directly from
+        NASA's API and a company events application to create, join and organize
+        internal events for employees. I would like to use my engineering skills
+        to build meaningful solutions to help sustain a healthier and better
+        future for planet Earth and humanity.
       </p>
       <br></br>
       <p>
@@ -37,12 +43,15 @@ function About() {
         received Magna Cum Laude Honors, I relocated to Denver, Colorado where I
         began my professional career. I became the youngest promoter in history
         to promote two large-scale charity events at the legendary Red Rocks
-        Amphitheater in Morrison, Colorado in 2013 and 2014. I then transitioned
-        into Real Estate and helped my team at TRELORA sell over 100 homes both
-        on the buying and listing sides. Eventually, I found my way back to the
-        music industry while pursuing my passion of music touring and artist
-        management before finding my new love and obsession in software
-        engineering.
+        Amphitheater in Morrison, Colorado in 2013 and 2014.
+      </p>
+      <br></br>
+      <p>
+        I then transitioned into Real Estate and helped my team at TRELORA sell
+        over 100 homes both on the buying and listing sides. Eventually, I found
+        my way back to the music industry while pursuing my passion of music
+        touring and artist management before finding my new love and obsession
+        in software engineering.
       </p>
       <br></br>
       <p>
