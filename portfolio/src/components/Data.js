f291afe507dba9a29a0372bff4df04582ced5966
21grahams <graham.kirsh@gmail.com>
2021-08-23 18:10:40 -0600

    all png files are loaded. Everything is rendering correctly for projects, just need to mess around with CSS and hover effects

--- portfolio/src/components/Data.js
+++ portfolio/src/components/Data.js
@@ -1,6 +1,7 @@
 import FE3 from "../img/FE3.png";
 import BlueOcean from "../img/BlueOcean.png";
 import SpaceJourney from "../img/SpaceJourney.png";
+import DavidWisdom from "../img/DavidApp.png";
 
 export const projects = [
   {
@@ -21,18 +22,18 @@ export const projects = [
   },
   {
     title: "Space Journey",
-    subtitle: "React and Postgres",
+    subtitle: "External APIs and backend functionality",
     description:
       "Your one stop destination for the newest and most intriguing pictures of space, Mars Rovers, Earth from above and more!",
     image: SpaceJourney,
     link: "http://bit.ly/GrahamsSpaceJourney",
   },
   {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
+    title: "David's Widsom App",
+    subtitle: "React and Postgres",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-4.gif",
-    link: "https://pythonbootcamp.com",
+      "davidOfWisdom is a simple user friendly application designed for my father to upload his many different quotes and thoughts he's shared with me and many others over the years. Each phrase gets stored in a Postgres database and a different unique phrase is selected at random with the click of a button. Have a friend, family member or colleague you think would enjoy capturing their unique ideas/thoughts/phrases and stored to relive and share over time? davidOfWisdom is the app for you!",
+    image: DavidWisdom,
+    link: "http://bit.ly/DavidsWisdomApp",
   },
 ];
