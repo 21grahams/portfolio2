25031f49b2b5fa1e18bfc21e81835da649aa2742
21grahams <graham.kirsh@gmail.com>
2021-08-25 15:58:21 -0600

    css effects added and centering pictures and title / description to DOM for projects

--- portfolio/src/components/Data.js
+++ portfolio/src/components/Data.js
@@ -6,23 +6,20 @@ import DavidWisdom from "../img/DavidApp.png";
 export const projects = [
   {
     title: "Project Catwalk",
-    subtitle: "FE-Three",
     description:
       "Project Catwalk is a front-end design for a ficticious e-commerce website. Users can browse through stylish clothing options and add them to their cart. They can also read through questions and answers submitted by previous shoppers. Users can even view product ratings and reviews and submit their own.",
     image: FE3,
-    link: "http://fe3catwalk.com",
+    link: "https://github.com/FE-Three/project-catwalk-v3",
   },
   {
     title: "Dean's Blue Ocean",
-    subtitle: "Attend Eaze",
     description:
-      "A company events application to create, join, and organize internal events for employees.",
+      "A company events application to create, join, and organize internal events for employees scheduled from employers.",
     image: BlueOcean,
-    link: "http://attendeaze.me/home",
+    link: "https://github.com/Deans-Blue-Ocean/blueocean",
   },
   {
     title: "Space Journey",
-    subtitle: "External APIs and backend functionality",
     description:
       "Your one stop destination for the newest and most intriguing pictures of space, Mars Rovers, Earth from above and more!",
     image: SpaceJourney,
@@ -30,7 +27,6 @@ export const projects = [
   },
   {
     title: "David's Widsom App",
-    subtitle: "React and Postgres",
     description:
       "davidOfWisdom is a simple user friendly application designed for my father to upload his many different quotes and thoughts he's shared with me and many others over the years. Each phrase gets stored in a Postgres database and a different unique phrase is selected at random with the click of a button. Have a friend, family member or colleague you think would enjoy capturing their unique ideas/thoughts/phrases and stored to relive and share over time? davidOfWisdom is the app for you!",
     image: DavidWisdom,
--- portfolio/src/contents/Projects.js
+++ portfolio/src/contents/Projects.js
@@ -5,28 +5,28 @@ function Projects() {
   return (
     <div className="condiv">
       <h1 className="subtopic">My Projects</h1>
-          {projects.map((project) => (
-            <a
-              href={project.link}
-              target="_blank"
-              rel="noreferrer noopener">
-              <div>
-                <img
-                  alt="FE3"
-                  src={project.image}
-                />
-                <div>
-                  <h2>
-                    {project.subtitle}
-                  </h2>
-                  <h1>
-                    {project.title}
-                  </h1>
-                  <p>{project.description}</p>
-                </div>
-              </div>
-            </a>
-          ))}
+      {projects.map((project, i) => (
+        <div>
+          <span className="projectTitle">{project.title}</span>
+          <p className="projectDescription">{project.description}</p>
+          <a
+            key={i}
+            href={project.link}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <span>
+              <img
+                className="projectPicture"
+                alt="projectpics"
+                src={project.image}
+              ></img>
+            </span>
+            <br></br>
+            <br></br>
+          </a>
+        </div>
+      ))}
     </div>
   );
 }
